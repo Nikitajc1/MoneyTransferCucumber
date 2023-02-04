8f3d09d6aa6a8ec537b604670bc83846f47cde35
Nikita U <nikitajc@mail.ru>
2023-02-04 23:06:32 +0300

    First

--- src/test/java/loginData/LoginPage.java
+++ src/test/java/loginData/LoginPage.java
@@ -1,32 +1,20 @@
-//package loginData;
-//
-//import com.codeborne.selenide.Condition;
-//import com.codeborne.selenide.SelenideElement;
-//
-//import static com.codeborne.selenide.Selenide.$;
-//
-//public class LoginPage {
-//
-//    private final SelenideElement login = $("[data-test-id=login] input");
-//    private final SelenideElement password = $("[data-test-id=password] input");
-//    private final SelenideElement buttonClick = $("[data-test-id=action-login]");
-//    private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
-//
-//    public void loginIn(String name, String pass) {
-//        login.setValue(name);
-//        password.setValue(pass);
-//        buttonClick.click();
-//    }
-//
-//    public void wrongLoginNotificationCheck() {
-//        errorNotification.should(Condition.appear).shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
-//    }
-//
-////    public static void validLoginCucumber(String name, String pass) {
-////        login.setValue(name);
-////        password.setValue(pass);
-////        buttonClick.click();
-////    }
-//
-//
-//}
+package loginData;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    private final SelenideElement login = $("[data-test-id=login] input");
+    private final SelenideElement password = $("[data-test-id=password] input");
+    private final SelenideElement buttonClick = $("[data-test-id=action-login]");
+
+
+    public void validLoginCucumber(String name, String pass) {
+        login.setValue(name);
+        password.setValue(pass);
+        buttonClick.click();
+    }
+}
