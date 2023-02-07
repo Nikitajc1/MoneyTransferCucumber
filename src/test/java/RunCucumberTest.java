800312e2d5fa1282e6fa5e8340426ded479c003e
Nikita U <nikitajc@mail.ru>
2023-02-07 12:41:52 +0300

    Fixed2

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,20 +1,11 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-//import org.junit.platform.suite.api.ConfigurationParameter;
-//import org.junit.platform.suite.api.IncludeEngines;
-//import org.junit.platform.suite.api.SelectClasspathResource;
-//import org.junit.platform.suite.api.Suite;
-//import steps.TemplateSteps;
-//
-//@Suite
-//@IncludeEngines("cucumber")
-//@SelectClasspathResource("src/test/java/resources/features")
-//@ConfigurationParameter(key = "MoneyTransferCucumber", value = "java.steps")
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "summary"},
         features = {"src/test/resources/features"},
-        glue = {"java.steps"})
+        glue = {"steps"})
 public class RunCucumberTest {
 }
